91009b6cd20f84876dafe646594737e5d3583cc6
Karam <qqqqkaramnaser@gmail.com>
2025-08-08 16:43:01 +0300

    Add Codemagic workflow configuration

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,18 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'package:irbid_basket/main.dart';
 
 void main() {
   testWidgets('App basic test', (WidgetTester tester) async {
+    // Ensure test binding is initialized and mock SharedPreferences so
+    // plugin calls in widget tree do not fail in tests/CI.
+    TestWidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences.setMockInitialValues({});
+
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
